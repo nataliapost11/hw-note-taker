1aa3cbafd16d7dce0a6f3414e925f1debdd15b23
Natalia Post <natalia.post11@gmail.com>
2021-08-24 00:17:40 -0400

    server.js changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const path = require('path');
 const api = require('./routes/index.js');
